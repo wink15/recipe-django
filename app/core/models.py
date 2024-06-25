7e2623c879e93be3fd3b9b22a618c0c6d1701e9f
Santi <santigiopo3@gmail.com>
2024-06-25 08:32:48 -0300

    test recipe fix

--- app/core/models.py
+++ app/core/models.py
@@ -2,49 +2,52 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin,
 )
 from django.conf import settings
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_field):
         if not email:
-            raise ValueError('User must have an email')
-        user=self.model(email=self.normalize_email(email), **extra_field)
+            raise ValueError("User must have an email")
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
 
-
         return user
+
     def create_superuser(self, email, password):
-        user= self.create_user(email, password)
-        user.is_staff=True
-        user.is_superuser=True
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
-    email=models.EmailField(max_length=255, unique=True)
-    name=models.CharField(max_length=255)
-    is_active=models.BooleanField(default=True)
-    is_staff= models.BooleanField(default=True)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
 
-    objects= UserManager()
+    objects = UserManager()
 
-    USERNAME_FIELD='email'
+    USERNAME_FIELD = "email"
+
 
 class Recipe(models.Model):
-    user= models.ForeignKey(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
 
-    title= models.CharField( max_length=200)
-    time_minute= models.IntegerField()
-    price= models.DecimalField( max_digits=5, decimal_places=2)
-    descripcion= models.TextField(blank=True)
-    link= models.CharField(blank=True, max_length=200)
+    title = models.CharField(max_length=200)
+    time_minute = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    descripcion = models.TextField(blank=True)
+    link = models.CharField(blank=True, max_length=200)
 
     def __str__(self):
         return self.title
--- app/recipe/test/test_recipe_api.py
+++ app/recipe/test/test_recipe_api.py
@@ -2,69 +2,66 @@ from decimal import Decimal
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-
 from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Recipe
 from recipe.serializers import RecipeSerializer
 
-RECIPE_URL=reverse('recipe:recipe-list')
+RECIPE_URL = reverse("recipe:recipe-list")
+
+
 def create_recipe(user, **params):
-    defaults={
-        'title': 'test example',
-        'time_minute': 22,
-        'price': Decimal('5.50'),
-        'descripcion':'Sample description',
-        'link':'http://example.com/recipe.pdf'
+    defaults = {
+        "title": "test example",
+        "time_minute": 22,
+        "price": Decimal("5.50"),
+        "descripcion": "Sample description",
+        "link": "http://example.com/recipe.pdf",
     }
 
     defaults.update(params)
-    recipe=Recipe.objects.create(user=user, **defaults)
+    recipe = Recipe.objects.create(user=user, **defaults)
     return recipe
 
 
 class PublicRecipeAPITests(TestCase):
 
     def setUp(self):
-        self.client=APIClient()
+        self.client = APIClient()
 
     def test_auth_required(self):
-        res=self.client.get(RECIPE_URL)
+        res = self.client.get(RECIPE_URL)
 
-        self.assertEqual(res.status.code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
 
 class PrivetRecipeAPITest(TestCase):
 
     def setUp(self):
-        self.client=APIClient()
-        self.user=get_user_model().objects.create_user(
-            'test@example.com',
-            'test123'
-        )
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user("test@example.com", "test123")
         self.client.force_authenticate(self.user)
-    
-    def  test_retrive_recipe(self):
-        create_recipe(user=self.user)
-        create_recipe(user=self.user)
-        res=self.client.get(RECIPE_URL)
 
-        recipes=Recipe.objects.all().order_by('-id')
-        serializer= RecipeSerializer(recipes, many=True)
-        self.assertEqual(res.status, status.HTTP_200_OK)
+    def test_retrive_recipe(self):
+        create_recipe(user=self.user)
+        create_recipe(user=self.user)
+        res = self.client.get(RECIPE_URL)
+
+        recipes = Recipe.objects.all().order_by("-id")
+        serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
-    
+
     def test_recipe_list_limited_to_user(self):
-        other_user=get_user_model().objects.create_user(
-            'test2@example.com',
-            'test1234'
+        other_user = get_user_model().objects.create_user(
+            "test2@example.com", "test1234"
         )
 
         create_recipe(user=other_user)
         create_recipe(user=self.user)
-        res=self.client.get(RECIPE_URL)
-        recipes=Recipe.objects.filter(user=self.user)
+        res = self.client.get(RECIPE_URL)
+        recipes = Recipe.objects.filter(user=self.user)
 
-        serializer=RecipeSerializer(recipes, many=True)
+        serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
-
